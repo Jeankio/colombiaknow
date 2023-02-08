740ade3d9c1b06bd4434b57b8c45182457dddaa2
Jean Karel <mailjeank@gmail.com>
2023-02-08 16:04:18 -0500

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Jean Karel on 8/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
